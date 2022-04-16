df237710b5de94b1e95afa703a908fb622b35838
rachit1307-code <71535060+rachit1307-code@users.noreply.github.com>
2022-04-16 13:09:50 -0700

    Update regex.py

--- regex.py
+++ regex.py
@@ -5,25 +5,25 @@ For example,
 str1 = "thisisaquestiontofindthelargestsubstring"
 str2 = "test"
 
-the output is
+The output is
 "thisisaquestiontofindthelargestsubst"
 
-explanation: you can find the "test" as below
+Explanation: you can find the "test" as below
 "t----------------------e----------st"
 """
 
 import re
 
-# input params
+# Input params
 str1 = "thisisaquestiontofindthelargestsubstring"
 str2 = "test"
 
-# data processing for regular expression
+# Data processing for regular expression
 str2_sample = [str2[-1]]*(len(str2))
 for i in range(len(str2)-1):str2_sample[i] = str2[i] + ".*"
 str2_sample = (''.join(str2_sample))
 
-# find the larget substring in str1
+# Find the larget substring in str1
 x = re.findall(str2_sample, str1)
 print (x)
 
